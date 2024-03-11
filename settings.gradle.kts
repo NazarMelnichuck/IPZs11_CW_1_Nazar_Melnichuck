c322f0814dc9640d3dea10730a375bc5e9760bcb
SayZ1X <m3m0ryjunior@gmail.com>
2024-03-11 12:14:48 +0200

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZs11_CW_1_Nazar_Melnichuck"
+include(":app")
+ 
